ac1a390832ed1bd2df5d4723e3385bcada5628c3
zhangwenh <11121020+wenhua111111@user.noreply.gitee.com>
2023-12-14 17:52:58 +0800

    t

--- 04-kNN/pythonProject/SimpleLinearRegression.py
+++ 04-kNN/pythonProject/SimpleLinearRegression.py
@@ -1,4 +1,5 @@
 import numpy as np
+from metrics import r2_score
 
 
 class SimpleLinearRegression1:
@@ -44,3 +45,51 @@ class SimpleLinearRegression1:
 
     def __repr__(self):
         return "SimpleLinearRegression()"
+
+
+class SimpleLinearRegression2:
+
+    def __init__(self):
+        """初始化Simple Linear Regression 模型"""
+        self.a_ = None
+        self.b_ = None
+
+    def fit(self, x_train, y_train):
+        """根据训练数据集x_train y_train训练Simple Linear Regression模型"""
+        assert x_train.ndim == 1, \
+            "simple"
+        assert len(x_train) == len(y_train), \
+            "the size of x_train must be equal to the size of y_train"
+
+        x_mean = np.mean(x_train)
+        y_mean = np.mean(y_train)
+
+        num = (x_train - x_mean).dot(y_train - y_mean)
+        d = (x_train - x_mean).dot(x_train - x_mean)
+
+        self.a_ = num / d
+        self.b_ = y_mean - self.a_ * x_mean
+
+        return self
+
+    def predict(self, x_predict):
+        """给定待预测数据集x_predict, 返回表示x_predict的结果向量"""
+        assert x_predict.ndim == 1, \
+            "Simple Linear Regression can only solve single feature train"
+        assert self.a_ is not None and self.b_ is not None, \
+            "must fit before predict"
+
+        return np.array([self._predict(x) for x in x_predict])
+
+    def _predict(self, x_single):
+        """给定单个待预测数据x_single 返回x_single的预测结果值"""
+        return self.a_ * x_single + self.b_
+
+    def score(self, x_test, y_test):
+        """根据测试数据集 x_test和y_test确定当前模型的准确度"""
+
+        y_predict = self.predict(x_test)
+        return r2_score(y_test, y_predict)
+
+    def __repr__(self):
+        return "SimpleLinearRegression2()"
--- 05-Liner-Regression/.ipynb_checkpoints/metrics-checkpoint.py
+++ 05-Liner-Regression/.ipynb_checkpoints/metrics-checkpoint.py
@@ -30,9 +30,3 @@ def mean_absolute_error(y_true, y_predict):
         "the size of y_true must be equal to the size of y_predict"
 
     return np.sum(np.absolute(y_true - y_predict)) / len(y_true)
-
-
-def r2_score(y_true, y_predict):
-    """计算y_true和y_predict之间的R Squre"""
-
-    return 1 - mean_squared_error(y_true, y_predict) / np.var(y_true)
